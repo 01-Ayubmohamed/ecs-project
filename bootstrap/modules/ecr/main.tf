839b3851c1073c6a8c9d7cc7012d56c551fa0cd3
Ayub Mohamed <ayub.mohamed9729@gmail.com>
2026-08-06 00:42:07 +0100

    fix: change ecr image tag to IMMUTABLE and remove unused moving target latest

--- bootstrap/modules/ecr/main.tf
+++ bootstrap/modules/ecr/main.tf
@@ -1,7 +1,7 @@
 
 resource "aws_ecr_repository" "main" {
   name                 = "${var.name}-ecr-repository"
-  image_tag_mutability = "MUTABLE"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
