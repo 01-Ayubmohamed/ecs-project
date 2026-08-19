d51164bf5d5fc5359ff9118c8d6dd4f86ccdf377
Ayub Mohamed <ayub.mohamed9729@gmail.com>
2026-08-19 19:44:38 +0100

    fix: add iam:ListInstanceProfilesForRole lost in deleted branch

--- bootstrap/modules/iam/main.tf
+++ bootstrap/modules/iam/main.tf
@@ -254,6 +254,7 @@ data "aws_iam_policy_document" "terraform_iam_management" {
       "iam:DetachRolePolicy",
       "iam:ListRolePolicies",
       "iam:ListAttachedRolePolicies",
+      "iam:ListInstanceProfilesForRole",
       "iam:PassRole",
     ]
     resources = [
